807483d671848035df78a6cfa9dd3e29055a7974
alicia <acamarac02@educarex.es>
2024-12-11 21:19:16 +0100

    Deploy website - based on 2af686dd52ebb3d2b50cda0aec86d789f3f80449

---
+++ assets/js/5c8b4b8c.b4ae5061.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpmdm=self.webpackChunkpmdm||[]).push([[716],{7558:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/docusaurus-prueba/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.4053c5be.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpmdm=self.webpackChunkpmdm||[]).push([[903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/docusaurus-prueba/blog","blogTitle":"Blog","authorsListPath":"/docusaurus-prueba/blog/authors"}')}}]);
